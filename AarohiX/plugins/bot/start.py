b1baa6f785992a5cf686cfa8b3f4d83ac64d65db
manoranjan23 <manoranjanhor43@gmail.com>
2024-12-24 11:31:22 +0530

    Update start.py

--- AarohiX/plugins/bot/start.py
+++ AarohiX/plugins/bot/start.py
@@ -127,7 +127,7 @@ async def start_pm(client, message: Message, _):
                 )
     else:
         out = private_panel(_)
-        await message.reply_sticker("CAACAgUAAyEFAASA8WLeAAKOlGdYXUAdDUJXOBYRUYwXZRzbhgQQAAJKFwACrkDJVgxj5NTrDQU8HgQ")
+        await message.reply_sticker("CAACAgUAAx0CfTISBwABAwNmZ2pNwXOL7xbpvQwfJRP5RAuI1aYAAuIKAALhihhX6D0H2P1wPz8eBA")
         await message.reply_photo(
             photo=config.START_IMG_URL,
             caption=_["start_2"].format(message.from_user.mention, app.mention),
